a6ade8493bbebc9a093791f4fba012ba3cfd33b6
Phoe-chan <ian2812@hotmail.com>
2013-01-07 12:10:18 +0000

    correcting misunderstanding about require and directories. Merging all routes into one file.

--- routes/board.js
+++
@@ -1,9 +0,0 @@
-
-/*
- * GET board display page.
- */
-
-exports.board = function(req, res){
-  var theBoardId = req.params.boardID;
-  res.render('board', { title: 'VBoard', boardId: theBoardId });
-};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,40 @@
 
 /*
- * GET home page.
+ * Application routes.
  */
 
 exports.index = function(req, res){
   res.render('index', { title: 'VBoard' });
+};
+
+exports.boardlist  = function(req, res){
+  res.json(['OK']);
+};
+
+exports.addboard  = function(req, res){
+  //database write to make new board.
+};
+
+exports.deleteboard  = function(req, res){
+  res.json(['OK']);
+};
+
+exports.imagelist  = function(req, res){
+  res.json(['OK']);
+};
+
+exports.boardlist  = function(req, res){
+  res.json(['OK']);
+};
+
+exports.addboard  = function(req, res){
+  //database write to make new board.
+};
+
+exports.deleteboard  = function(req, res){
+  res.json(['OK']);
+};
+
+exports.imagelist  = function(req, res){
+  res.json(['OK']);
 };
